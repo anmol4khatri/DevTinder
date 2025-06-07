731f7034ec0a833a11e6a372e5adc46a6ab91443
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-06-07 14:07:35 +0530

    added meaningful json response in PATCH "/profile/edit"
    added TODO for PATCH "/profile/chnagePassword"

--- DevTinder Backend/src/routes/profileRouter.js
+++ DevTinder Backend/src/routes/profileRouter.js
@@ -5,31 +5,48 @@ const User = require("../models/user");
 const profileRouter = express.Router();
 
 //User Profile
-profileRouter.get("/profile/view", userAuth, (req,res) => {
-    try{
+profileRouter.get("/profile/view", userAuth, (req, res) => {
+    try {
         const user = req.user;//comes form userAuth middleware
         res.status(200).send(user);
-    } catch(err){
+    } catch (err) {
         res.status(500).send("Error: " + err.message);
     }
 });
 
-//Update the profile details
+//Update the profile details(with restriction)
 profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     try {
         const loggedInUser = req.user; //comes from userAuth middleware
         const editAllowedFields = ["firstName", "lastName", "emailId", "photoUrl", "about", "skills"];
 
         Object.keys(req.body).forEach((key) => {
-            if(editAllowedFields.includes(key)) {
+            if (editAllowedFields.includes(key)) {
                 loggedInUser[key] = req.body[key];
             }
         });
         await loggedInUser.save();
-        res.status(200).send("User Updated Successfully");
+        res.json({
+            message: `${loggedInUser.firstName}, your profile updated successfuly`,
+            data: loggedInUser,
+        });
     } catch (err) {
         res.status(500).send("Error: " + err.message);
     }
+});
+
+//Password Change
+profileRouter.patch("/profile/changePassword", userAuth, async (req, res) => {
+    // TODO: 
+    // input(res.body) ==> currentPassword, New Password, Coinform Password
+    // none of the field shoud be empty
+    // new password should exactly match coinform password
+    // password data validation
+    // bcrypt comapre ==> currentPAssword & newPassword
+    // throw error if not
+    // if true, hash new passowrd and store it to the DB
+    // generate a new jwt token
+    // overwrite the cookie to store new token
 });
 
 module.exports = profileRouter;
