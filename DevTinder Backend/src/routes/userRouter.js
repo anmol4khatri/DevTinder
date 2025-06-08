131fb7d3567cb88c46416b6d4ac5825e84ff0c0c
Anmol Khatri <100636635+anmol4khatri@users.noreply.github.com>
2025-06-08 18:24:55 +0530

    Enhance connection request model and user router with new endpoints
    Updated the connectionRequest model to include a reference to the User model for both fromUserId and toUserId fields. Added new GET endpoints in userRouter to retrieve accepted connections and pending requests, improving user interaction with connection functionalities. Enhanced error handling for empty responses.

--- DevTinder Backend/src/routes/userRouter.js
+++ DevTinder Backend/src/routes/userRouter.js
@@ -1,7 +1,51 @@
 const express = require("express");
+const ConnectionRequest = require("../models/connectionRequest");
 const User = require("../models/user");
 const { userAuth } = require("../middlewares/auth");
+
 const userRouter = express.Router();
+
+// See all connections
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+    try {
+        const connections = await ConnectionRequest.find({
+            $or:[
+                {toUserId: req.user._id, status: "accepted"},
+                {fromUserId: req.user._id, status: "accepted"}
+            ]
+        })
+        .populate("fromUserId", "firstName lastName photoUrl age gender")
+        .populate("toUserId", "firstName lastName photoUrl age gender");
+        if(!connections || connections.length === 0) throw new Error("No connections yet");
+
+        const data = connections.map((row) => {
+            if(row.fromUserId._id.toString() === req.user._id.toString()){
+                return row.toUserId;
+            }
+            return row.fromUserId;
+        });
+
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({error: "Error: " + err.message})
+    }
+});
+
+// Pending connection request
+userRouter.get("/user/requests", userAuth, async (req, res) => {
+    try {
+        const requests = await ConnectionRequest.find({
+            toUserId: req.user._id,
+            status: "intrested",
+        })
+        .populate("fromUserId", "firstName lastName photoUrl age gender");
+        if(!requests || requests.length === 0) throw new Error("No requests yet");
+
+        res.status(200).json(requests);
+    } catch (err) {
+        res.status(500).json({error: "Error: " + err.message})
+    }
+});
 
 //Get all users
 userRouter.get("/user/feed", userAuth, async (req, res) => {
@@ -13,5 +57,6 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         res.status(500).send("Error: " + err.message);
     }
 });
+
 
 module.exports = userRouter;
